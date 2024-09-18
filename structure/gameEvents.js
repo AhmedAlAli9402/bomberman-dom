da6454fc2a3593d0921fff62b9ddac0cf5fa0bae
AhmedAlAli9402 <ahmedalali5294@gmail.com>
2024-09-18 15:06:19 +0300

    changed logic for breakwall

--- structure/gameEvents.js
+++ structure/gameEvents.js
@@ -7,45 +7,26 @@ import { sendplayerGameOver, sendKillPlayer } from '../app.js';
 
 let players = game.players;
 
-
-export function breakWall(id) {
+export function breakWall(id, directions) {
     let bomb = document.getElementById(id);
-    let powerbomb = false
-    if (!bomb.classList.contains('bomb')) {
-        bomb = document.querySelector('.powerBombDropped');
-        powerbomb = true;
-    }
-    if (!bomb) return;
     const bombPosition = Number(bomb.getAttribute('id'));
-    let directions = [-1, 1, width, -width];
-
-    if (powerbomb) {
-        let powerbombDirections = [-2, 2, width*2, -width*2];
-        for (let i = 0; i < 4; i++) {
-            if (!availableSquares[bombPosition + directions[i]].classList.contains('wall')) {
-                directions.push(powerbombDirections[i]);
-            }
-        }
-        }
-
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < directions.length; i++) {
        const square = availableSquares[bombPosition + directions[i]];
         if (square && square.classList.contains('breakableWall')) {
             square.classList.replace('breakableWall', 'breakWall');
+            game.gameGrid.breakableWall = game.gameGrid.breakableWall.filter(wall => wall !== bombPosition + directions[i]);
             setTimeout(() => square.classList.remove('breakWall'), 500);
         } else if (square && !square.classList.length) {
             square.classList.add("sideExplosion");
             setTimeout(() => square.classList.remove("sideExplosion"), 200);
         }
     };
-
     bomb.classList.replace("bomb", 'explosion');
-    if (powerbomb) {
+    if (bomb.classList.contains('powerBombDropped')) {
         bomb.classList.replace('powerBombDropped', 'explosion');
     }
     setTimeout(() => bomb.classList.remove('explosion'), 200);
     console.log(directions)
-    return directions;
 }
 
 
@@ -84,4 +65,20 @@ export function killPlayer(userId) {
 }
     ;}, 10)
     updateHUD(userId)
+}
+
+export function checkGameStructure(gameGrid){
+    console.log(gameGrid)
+    console.log(game.gameGrid)
+    if (gameGrid.breakableWall.length < game.gameGrid.breakableWall.length) {
+        game.gameGrid.breakableWall.map(wall => {if (!gameGrid.breakableWall.includes(wall)) {
+            document.getElementById(wall).classList.remove('breakableWall');}
+        });
+        game.gameGrid.breakableWall = gameGrid.breakableWall;
+    }
+    if (gameGrid.powerUps.length < game.gameGrid.powerUps.length) {
+        game.gameGrid.powerUps.map(powerUps => {if (!gameGrid.powerUps.includes(powerUps)) {
+            document.getElementById(powerUps.index).classList.remove(powerUps.powerUp);}});
+        game.gameGrid.powerUps = gameGrid.powerUps;
+    }
 }
