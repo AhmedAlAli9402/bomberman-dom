f271f2f542b54b4f14df55d6bdb1e75717ee3159
AhmedAlAli9402 <ahmedalali5294@gmail.com>
2024-09-18 15:08:55 +0300

    added a way to handle the powerUps in the backend

--- ws-test-vanilla-js/handlemessages.js
+++ ws-test-vanilla-js/handlemessages.js
@@ -1,96 +1,136 @@
-import { calculateNewPosition, isMoveValid } from "./calculatepos.js";
+import {
+  calculateNewPosition,
+  HandleExplosion,
+  isMoveValid,
+  positionToIndex,
+} from "./calculatepos.js"
 
 export function handleMessages(data, ws, currentGame) {
   if (currentGame.clients.has(ws) && data.message) {
-    const nickname = currentGame.clients.get(ws);
-    const playerId = Array.from(currentGame.clients.keys()).indexOf(ws);
-    const player = currentGame.players[playerId];
-    let broadcast = {};
-
+    const nickname = currentGame.clients.get(ws)
+    const playerId = Array.from(currentGame.clients.keys()).indexOf(ws)
+    const player = currentGame.players[playerId]
+    let broadcast = {}
     if (data.message.messageType === "move") {
-      const { direction } = data.message;
+      // console.log(data.message.messageType);
+      const { direction } = data.message
       // const playerPosition = player.playerPosition;
-      player.startPosition = player.playerPosition;
+      player.startPosition = player.playerPosition
       // Calculate new position based on direction
       const newPosition = calculateNewPosition(
         player.playerPosition,
         direction,
         currentGame.gameGrid
-      );
+      )
 
       // Validate the move (e.g., check if the new position is free)
       if (isMoveValid(newPosition, currentGame)) {
-        player.playerPosition = newPosition;
-
+        player.playerPosition = newPosition
+        let playerIndex = positionToIndex(newPosition, currentGame.gameGrid)
+        if (currentGame.gameGrid.powerUpsIndex.includes(playerIndex)) {
+          // if (player.powerUp !== "") {
+          //   clearTimeout(powerUpTimeOut);
+          // }
+          const currentPowerUp = currentGame.gameGrid.powerUp.filter((power) => power.index === playerIndex).map((power) => power.powerUp)
+          if (currentPowerUp) {
+          player.powerUp = currentPowerUp[0]
+          }
+          setTimeout(() => {
+            player.powerUp = ""
+          }, 20000)
+          currentGame.gameGrid.powerUp = currentGame.gameGrid.powerUp.filter(
+            (powerUp) => powerUp.index !== playerIndex
+          )
+          currentGame.gameGrid.powerUpsIndex =
+            currentGame.gameGrid.powerUpsIndex.filter(
+              (powerUp) => powerUp !== playerIndex
+            )
+          console.log(
+            "powerUps",
+            currentGame.gameGrid.powerUp,
+            "indoooooec",
+            currentGame.gameGrid.powerUpsIndex
+          )
+        }
         broadcast = {
           messageType: "updatePosition",
           id: playerId,
           currentGame: currentGame,
           direction: direction,
-        };
+        }
       } else {
         broadcast = {
           messageType: "invalidMove",
           id: playerId,
           currentGame: currentGame,
           direction: direction,
-        };
+        }
       }
     } else if (data.message.messageType === "keyUp") {
       broadcast = {
         messageType: "keyUp",
         id: playerId,
-      };
+      }
     } else if (data.message.messageType === "placeBomb") {
-      if ((player.bombDropped >= 1 && player.powerUp !== "extraBomb") || (player.bombDropped >= 2 && player.powerUp === "extraBomb")) return;
-      player.bombDropped++;
-
+      console.log("bombDropped", player.bombDropped, player.powerUp)
+      if (
+        (player.bombDropped >= 1 && player.powerUp !== "extraBomb") ||
+        (player.bombDropped >= 2 && player.powerUp === "extraBomb")
+      )
+        return
+      player.bombDropped++
       broadcast = {
         messageType: "placeBomb",
         id: playerId,
-      };
+        currentGame: currentGame,
+      }
       setTimeout(() => {
-        player.bombDropped--;
+        currentGame.gameGrid.breakableWall = HandleExplosion(
+          playerId,
+          player.playerPosition,
+          currentGame
+        )
+        // console.log(currentGame.gameGrid.breakableWall);
+        player.bombDropped--
         broadcast = {
           messageType: "bombExplosion",
           id: playerId,
-        };
-      }
-      , 3000);
+        }
+      }, 3000)
     } else if (data.message.messageType === "killPlayer") {
       broadcast = {
         messageType: "killPlayer",
         id: data.message.userId,
-      };
+      }
     } else if (data.message.messageType === "chat") {
-      currentGame.chatMessages.push(`${nickname}: ${data.message.message}`); // Store the message
+      currentGame.chatMessages.push(`${nickname}: ${data.message.message}`) // Store the message
       broadcast = {
         messageType: "chat",
         nickname: nickname,
         message: data.message,
-      };
+      }
     } else if (data.message.messageType === "chatTest") {
       broadcast = {
         messageType: "chatTest",
         nickname: nickname,
         message: data.message,
-      };
+      }
     }
 
     const updateGameState = {
       messageType: "updateGameState",
       currentGame: currentGame,
-    };
+    }
 
     // Broadcast the message to all connected clients
     if (broadcast || updateGameState) {
       for (const client of currentGame.clients.keys()) {
         if (client.readyState === ws.OPEN) {
           // client.send(JSON.stringify(updateGameState));
-          client.send(JSON.stringify(broadcast));
+          client.send(JSON.stringify(broadcast))
         }
       }
     }
-    return;
+    return
   }
 }
